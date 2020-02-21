2b2c6a609f0151b887fea15a7353fd6a4602cdd7
samrat <samratasp@gmail.com>
2020-02-21 16:47:44 -0600

    assignment 2 completed

---
+++ 3reducer.py
@@ -0,0 +1,28 @@
+s = open("02.txt","r",encoding='utf-8')
+r = open("maxdata.txt", "w",encoding='utf-8')
+
+thisKey = ""
+thisValue = 0.0
+
+for line in s:
+  data = line.strip().split('\t')
+  channel_title, likes = data
+  print(line)
+
+  if channel_title != thisKey:
+    if thisKey:
+      # output the last key value pair result
+      r.write(thisKey + '\t' + str(thisValue)+'\n')
+
+    # start over when changing keys
+    thisKey = channel_title 
+    thisValue = 0.0
+  
+  # apply the aggregation function
+  thisValue = max(thisValue,float(likes))
+
+# output the final entry when done
+r.write(thisKey + '\t' + str(thisValue)+'\n')
+
+s.close()
+r.close()
