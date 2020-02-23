23722b21faa2ae40f6ecee32010fa25b5e3d391c
Rahulmallampati825 <rahulmallampati825@gmail.com>
2020-02-23 15:07:04 -0600

    converted output file from txt file to csv file

--- Rahul/3reducer.py
+++ Rahul/3reducer.py
@@ -1,23 +1,18 @@
 s = open("min2.txt","r",encoding='utf-8')
-r = open("minoutput.txt","w",encoding='utf-8')
+r = open("minoutput.csv","w",encoding='utf-8')
 
 thisKey = ""
 thisValue = 9999999.0
 
 for line in s:
   data = line.strip().split('\t')
-<<<<<<< HEAD:Rahul/3reducer.py
   channel_title, views = data
  
-=======
-  channel_title, likes = data
-  #print(line)
->>>>>>> 893778d8b9d8a854bc562307d2643136e765fdff:saisamrat/3reducer.py
 
   if channel_title != thisKey:
     if thisKey:
       # output the last key value pair result
-      r.write(thisKey + '\t' + str(thisValue)+'\n')
+      r.write(thisKey + ',' + str(thisValue)+'\n')
 
     # start over when changing keys
     thisKey = channel_title 
@@ -28,7 +23,7 @@ for line in s:
   thisValue = min(thisValue,float(views))
 
 # output the final entry when done
-r.write(thisKey + '\t' + str(thisValue)+'\n')
+r.write(thisKey + ',' + str(thisValue)+'\n')
 
 s.close()
 r.close()
