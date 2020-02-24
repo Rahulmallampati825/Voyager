818cce795c90eccd3d29de8d26789a9f7e20f0bd
Revanth <revanth.d1995@gmail.com>
2020-02-24 14:39:55 -0600

    updated screenshots in image folder and reducer

--- Revanth/3reducer.py
+++ Revanth/3reducer.py
@@ -1,4 +1,4 @@
-s = open("avg2.txt","r",encoding='utf-8')
+s = open("avg1.txt","r",encoding='utf-8')
 r = open("avgdata.txt", "w",encoding='utf-8')
 
 thisKey = ""
@@ -7,7 +7,7 @@ thisValue = 0.0
 for line in s:
   data = line.strip().split('\t')
   channel_title, likes = data
-  print(line)
+  
 
   if channel_title != thisKey:
     if thisKey:
